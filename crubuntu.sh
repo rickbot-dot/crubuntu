e05cd9d8762332237bee83ea37e65c13972998bb
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-08-07 23:15:37 +0100

    Update crubuntu.sh

--- crubuntu.sh
+++ crubuntu.sh
@@ -8,9 +8,9 @@ source sh/menu
 # Clear the screen for more clean interface
 clear
 
-cecho yellow "Crubuntu 23.04 Setup"
-echo "-----------------------"
-cat msg/welcome
+cecho white "Crubuntu 23.04 Setup"
+echo
+cat cat msg/welcome
 echo
 options=("Install Crubuntu and Remove Debian" "Remove Crubuntu and Install Debian" "Wiki" "Google (for help)" "Exit Setup" "[Advanced] Termina Shell")
 

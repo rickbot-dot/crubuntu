584f132e53aa309b3bb7fa34e1d006c7a8a23561
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-08-07 23:05:00 +0100

    Update crubuntu.sh

--- crubuntu.sh
+++ crubuntu.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-source sh/colors.sh
-echo "Crubuntu"
+# Load scripts
+source sh/colors
+source sh/menu
+
+# Clear the screen for more clean interface
+clear
+
+cecho yellow "Crubuntu 23.04 Setup"
+echo "-----------------------"
+cat msg/welcome
+echo
+options=("Install Crubuntu and Remove Debian" "Remove Crubuntu and Install Debian" "Wiki" "Exit")
+
+select_option "${options[@]}"
+choice=$?
+
+echo "Choosen index = $choice"
+echo "        value = ${options[$choice]}"

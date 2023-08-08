d8a5ee1bf8e57f4ef6d6fc0d1ba8cbfccc3cb953
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-08-08 14:33:38 +0100

    Update crubuntu.sh

--- crubuntu.sh
+++ crubuntu.sh
@@ -15,8 +15,6 @@ options=("Install Crubuntu" "Remove Crubuntu" "Help (Wiki, Google, etc)" "Exit S
 
 select_option "${options[@]}"
 choice=$?
-echo $choice
-sleep 2
 
 clear
 
@@ -40,12 +38,21 @@ cecho yellow "Crubuntu 23.04 Setup"
 echo
 cat msg/uninstaller
 echo
-options=("< Back" "Uninstall Crubuntu")
+read uninstall_conf -e
+if [[ $uninstall_conf == "CRUBUNTU" ]]; then
+echo "CRUBUNTU was entered!"
+echo "All data in the Crubuntu container will be removed."
+echo "This action is permanent and cannot be undone. Consider a backup first."
+echo "The user account you created will not be normal, and will be root,"
+echo "so you will need to create a new user and make it the default."
+echo "You will need to create a new container for Crostini."
+echo "On some setups Crostini may appear incompatible without a backup container."
+echo "Do you really want to remove Crubuntu?"
+options=("No" "Yes")
 
 select_option "${options[@]}"
-choice=$?
-echo $choice
-sleep 2
+if [[ $? == 1 ]]; then
+   lxc remove penguin
 fi
 
 # Exit
@@ -62,5 +69,24 @@ echo
 read -N 1 -s -t 10
 fi
 
+# Advanced
+if [[ $choice == 4 ]]; then
+while true; do
+cecho red "Crubuntu 23.04 Setup"
+echo
+options=("Crostini Shell" "< Back")
+
+select_option "${options[@]}"
+choice=$?
+
 clear
+
+if [[ $choice == 0 ]]; then
+lxc exec penguin -- bash
+fi
+if [[ $choice == 1 ]]; then
+break
+fi
+done
+fi
 done

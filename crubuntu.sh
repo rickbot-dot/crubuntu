1c62b0d4c9f9e43dc97a92a21bd76ff28270c8a1
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-08-08 14:34:57 +0100

    Update crubuntu.sh

--- crubuntu.sh
+++ crubuntu.sh
@@ -2,91 +2,93 @@
 source sh/colors
 source sh/menu
 
-# Clear the screen for more clean interface
+# Clear the screen for a cleaner interface
 clear
 
 # Menu
 while true; do
-cecho yellow "Crubuntu 23.04 Setup"
-echo
-cat msg/welcome
-echo
-options=("Install Crubuntu" "Remove Crubuntu" "Help (Wiki, Google, etc)" "Exit Setup" "Advanced")
+  cecho yellow "Crubuntu 23.04 Setup"
+  echo
+  cat msg/welcome
+  echo
+  options=("Install Crubuntu" "Remove Crubuntu" "Help (Wiki, Google, etc)" "Exit Setup" "Advanced")
 
-select_option "${options[@]}"
-choice=$?
+  select_option "${options[@]}"
+  choice=$?
 
-clear
+  clear
 
-# Installer
-if [[ $choice == 0 ]]; then
-cecho yellow "Crubuntu 23.04 Setup"
-echo
-cat msg/installer
-echo
-options=("Install Crubuntu 23.04 (Lunar / stable)" "Install Crubuntu 22.04 LTS (Jammy)" "Install Crubuntu 20.04 LTS (Focal)" "Install Crubuntu 23.10 Development Build (Lobster / unstable)" "< Back")
+  # Installer
+  if [[ $choice == 0 ]]; then
+    cecho yellow "Crubuntu 23.04 Setup"
+    echo
+    cat msg/installer
+    echo
+    options=("Install Crubuntu 23.04 (Lunar / stable)" "Install Crubuntu 22.04 LTS (Jammy)" "Install Crubuntu 20.04 LTS (Focal)" "Install Crubuntu 23.10 Development Build (Lobster / unstable)" "< Back")
 
-select_option "${options[@]}"
-choice=$?
-echo $choice
-sleep 2
-fi
+    select_option "${options[@]}"
+    choice=$?
+    echo $choice
+    sleep 2
+  fi
 
-# Uninstaller
-if [[ $choice == 1 ]]; then
-cecho yellow "Crubuntu 23.04 Setup"
-echo
-cat msg/uninstaller
-echo
-read uninstall_conf -e
-if [[ $uninstall_conf == "CRUBUNTU" ]]; then
-echo "CRUBUNTU was entered!"
-echo "All data in the Crubuntu container will be removed."
-echo "This action is permanent and cannot be undone. Consider a backup first."
-echo "The user account you created will not be normal, and will be root,"
-echo "so you will need to create a new user and make it the default."
-echo "You will need to create a new container for Crostini."
-echo "On some setups Crostini may appear incompatible without a backup container."
-echo "Do you really want to remove Crubuntu?"
-options=("No" "Yes")
+  # Uninstaller
+  if [[ $choice == 1 ]]; then
+    cecho yellow "Crubuntu 23.04 Setup"
+    echo
+    cat msg/uninstaller
+    echo
+    read uninstall_conf -e
+    if [[ $uninstall_conf == "CRUBUNTU" ]]; then
+      echo "CRUBUNTU was entered!"
+      echo "All data in the Crubuntu container will be removed."
+      echo "This action is permanent and cannot be undone. Consider a backup first."
+      echo "The user account you created will not be normal, and will be root,"
+      echo "so you will need to create a new user and make it the default."
+      echo "You will need to create a new container for Crostini."
+      echo "On some setups Crostini may appear incompatible without a backup container."
+      echo "Do you really want to remove Crubuntu?"
+      options=("No" "Yes")
 
-select_option "${options[@]}"
-if [[ $? == 1 ]]; then
-   lxc remove penguin
-fi
+      select_option "${options[@]}"
+      if [[ $? == 1 ]]; then
+        lxc remove penguin
+      fi
+    fi
+  fi
 
-# Exit
-if [[ $choice == 3 ]]; then
-exit
-fi
+  # Exit
+  if [[ $choice == 3 ]]; then
+    exit
+  fi
 
-# Help
-if [[ $choice == 2 ]]; then
-cecho yellow "Crubuntu 23.04 Setup"
-echo
-cat msg/help
-echo
-read -N 1 -s -t 10
-fi
+  # Help
+  if [[ $choice == 2 ]]; then
+    cecho yellow "Crubuntu 23.04 Setup"
+    echo
+    cat msg/help
+    echo
+    read -N 1 -s -t 10
+  fi
 
-# Advanced
-if [[ $choice == 4 ]]; then
-while true; do
-cecho red "Crubuntu 23.04 Setup"
-echo
-options=("Crostini Shell" "< Back")
+  # Advanced
+  if [[ $choice == 4 ]]; then
+    while true; do
+      cecho red "Crubuntu 23.04 Setup"
+      echo
+      options=("Crostini Shell" "< Back")
 
-select_option "${options[@]}"
-choice=$?
+      select_option "${options[@]}"
+      choice=$?
 
-clear
+      clear
 
-if [[ $choice == 0 ]]; then
-lxc exec penguin -- bash
-fi
-if [[ $choice == 1 ]]; then
-break
-fi
-done
-fi
+      if [[ $choice == 0 ]]; then
+        lxc exec penguin -- bash
+      fi
+      if [[ $choice == 1 ]]; then
+        break
+      fi
+    done
+  fi
 done
